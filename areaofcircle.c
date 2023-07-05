0d20b8285e4d7b2f8686a821b4d92b4356361140
Hensi Kavar <115384276+hensikavar@users.noreply.github.com>
2023-07-05 17:34:38 +0530

    C program to find area of circle.

---
+++ areaofcircle.c
@@ -0,0 +1,14 @@
+// Area of circle.
+#include<stdio.h>
+#define pi 3.14
+
+void main()
+{
+	float r;
+	
+	printf("Enter the value of radius :");
+	scanf("%f",&r);
+	
+	printf("Area of the circle is %.2f",pi*r*r);
+	return;
+}
