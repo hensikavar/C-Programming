17970554580ba0785636ff51fbd68bc8c5d4d04c
Hensi Kavar <115384276+hensikavar@users.noreply.github.com>
2023-08-19 11:36:30 +0530

    C program to print multiplication table using For loop.

---
+++ fortable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+	int i,n;
+	
+	printf("Enter the number - ");
+	scanf("%d",&n);
+	
+	for(i=1;i<=10;i++)
+	{
+		printf("%d x %d = %d\n",n,i,n*i);
+	}
+	
+	return;
+}
