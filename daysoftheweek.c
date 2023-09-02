ba3904f28484ac30a302c34910ff1173ad5a74d9
Hensi Kavar <115384276+hensikavar@users.noreply.github.com>
2023-09-02 21:25:00 +0530

    C program to find name of the week for given day.

---
+++ daysoftheweek.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void main()
+{
+	int n;
+	
+	a:
+	{
+		printf("Enter the number :");
+		scanf("%d",&n);
+	}
+	
+	switch(n)
+	{
+		case 1:printf("Sunday");
+		break;
+		case 2:printf("Monday");
+		break;
+		case 3:printf("Tuesday");
+		break;
+		case 4:printf("Wednesday");
+		break;
+		case 5:printf("Thursday");
+		break;
+		case 6:printf("Friday");
+		break;
+		case 7:printf("Saturday");
+		break;
+		
+		default:
+			printf("Incorrect Input\n");
+			goto a;
+	}
+}
